5dc5deaebe699092001f78cc5918e126239c76af
ebarnas <ebarnas@students.mchenry.edu>
2026-01-26 22:47:36 -0600

    Initial Commit

---
+++ assessment-01/scripts/scripts.js
@@ -0,0 +1,15 @@
+function changeImage1(){
+    document.getElementById("hobby").src="images/bannerlord.jpg";
+}
+
+function changeImage2(){
+    document.getElementById("hobby").src="images/counterstrike.jpg";
+}
+
+function changeImage3(){
+    document.getElementById("hobby").src="images/my-cat.jpg";
+}
+
+function hobbyFunction3(){
+    document.getElementById("hobby-info").innerHTML="I have a cat named Sebastien. I pet him everyday as my hobby";
+}
